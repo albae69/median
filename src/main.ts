8b52564059ad199c3eee3e6ea077d8b2a3ad2c7e
albae69 <ahmadalbaihaqi69@gmail.com>
2023-09-07 13:51:14 +0700

    added: error handler

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { PrismaClientExpectionFilter } from './prisma-client-exception/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,10 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExpectionFilter(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
